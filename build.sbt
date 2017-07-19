916d93399005866269f9fc5466f5e970e151aac5
lewishayqa <lewis.hay@qa.com>
2017-07-19 11:08:41 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "AtbashCipher"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ src/main/scala/Atbash.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Administrator on 19/07/2017.
+  */
+class Atbash {
+  val plainText = "abcdefghijklmnopqrstuvwxyz"
+  val cipherText = plainText.reverse
+
+  def encode(text: String) = {
+
+  }
+}
